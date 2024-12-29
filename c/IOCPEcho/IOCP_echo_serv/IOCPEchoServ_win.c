f50fa2258f5a5b774097ae57e891d370f9c319bf
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-12-29 19:59:06 +0900

    수정

--- c/IOCPEcho/IOCP_echo_serv/IOCPEchoServ_win.c
+++ c/IOCPEcho/IOCP_echo_serv/IOCPEchoServ_win.c
@@ -1,34 +1,35 @@
 #pragma comment(lib, "ws2_32.lib")
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <process.h>
 #include <winsock2.h>
 #include <windows.h>
 
 #define BUF_SIZE 100
-#define READ 3
-#define WRITE 5
+#define READ	3
+#define	WRITE	5
 
-typedef struct
+typedef struct    // socket info
 {
 	SOCKET hClntSock;
 	SOCKADDR_IN clntAdr;
-}PER_HANDLE_DATA, *LPPER_HANDLE_DATA;
+} PER_HANDLE_DATA, * LPPER_HANDLE_DATA;
 
-typedef struct
+typedef struct    // buffer info
 {
 	OVERLAPPED overlapped;
 	WSABUF wsaBuf;
 	char buffer[BUF_SIZE];
-	int rwMode; //READ or WRITE
-}PER_IO_DATA,*LPPER_IO_DATA;
+	int rwMode;    // READ or WRITE
+} PER_IO_DATA, * LPPER_IO_DATA;
 
 DWORD WINAPI EchoThreadMain(LPVOID CompletionPortIO);
-void ErrorHandling(char* msg);
+void ErrorHandling(char* message);
 
 int main(int argc, char* argv[])
 {
-	WSADATA wsaData;
+	WSADATA	wsaData;
 	HANDLE hComPort;
 	SYSTEM_INFO sysInfo;
 	LPPER_IO_DATA ioInfo;
@@ -37,16 +38,15 @@ int main(int argc, char* argv[])
 	SOCKET hServSock;
 	SOCKADDR_IN servAdr;
 	int recvBytes, i, flags = 0;
+
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup error");
+		ErrorHandling("WSAStartup() error!");
 
 	hComPort = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
 	GetSystemInfo(&sysInfo);
+
 	for (i = 0; i < sysInfo.dwNumberOfProcessors; i++)
-	{
-		printf("process!\n");
 		_beginthreadex(NULL, 0, EchoThreadMain, (LPVOID)hComPort, 0, NULL);
-	}
 
 	hServSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 	memset(&servAdr, 0, sizeof(servAdr));
@@ -64,6 +64,7 @@ int main(int argc, char* argv[])
 		int addrLen = sizeof(clntAdr);
 
 		hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &addrLen);
+
 		handleInfo = (LPPER_HANDLE_DATA)malloc(sizeof(PER_HANDLE_DATA));
 		handleInfo->hClntSock = hClntSock;
 		memcpy(&(handleInfo->clntAdr), &clntAdr, addrLen);
@@ -75,7 +76,9 @@ int main(int argc, char* argv[])
 		ioInfo->wsaBuf.len = BUF_SIZE;
 		ioInfo->wsaBuf.buf = ioInfo->buffer;
 		ioInfo->rwMode = READ;
-		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf), 1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
+
+		WSARecv(handleInfo->hClntSock, &(ioInfo->wsaBuf),
+			1, &recvBytes, &flags, &(ioInfo->overlapped), NULL);
 	}
 	return 0;
 }
@@ -91,44 +94,46 @@ DWORD WINAPI EchoThreadMain(LPVOID pComPort)
 
 	while (1)
 	{
-		GetQueuedCompletionStatus(hComPort, &bytesTrans, (LPDWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
+		GetQueuedCompletionStatus(hComPort, &bytesTrans,
+			(LPDWORD)&handleInfo, (LPOVERLAPPED*)&ioInfo, INFINITE);
 		sock = handleInfo->hClntSock;
 
 		if (ioInfo->rwMode == READ)
 		{
-			puts("message received");
-			if (bytesTrans == 0) //EOF일시
+			puts("message received!");
+			if (bytesTrans == 0)    // EOF 전송 시
 			{
 				closesocket(sock);
-				free(handleInfo);
-				free(ioInfo);
+				free(handleInfo); free(ioInfo);
 				continue;
 			}
-			printf("Allocating IO data.\n");
+
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsaBuf.len = bytesTrans;
 			ioInfo->rwMode = WRITE;
-			WSASend(sock, &(ioInfo->wsaBuf), 1, NULL, 0, &(ioInfo->overlapped), NULL);
+			WSASend(sock, &(ioInfo->wsaBuf),
+				1, NULL, 0, &(ioInfo->overlapped), NULL);
 
 			ioInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
 			memset(&(ioInfo->overlapped), 0, sizeof(OVERLAPPED));
 			ioInfo->wsaBuf.len = BUF_SIZE;
 			ioInfo->wsaBuf.buf = ioInfo->buffer;
 			ioInfo->rwMode = READ;
-			WSARecv(sock, &(ioInfo->wsaBuf), 1, NULL, &flags, &(ioInfo->overlapped), NULL);
+			WSARecv(sock, &(ioInfo->wsaBuf),
+				1, NULL, &flags, &(ioInfo->overlapped), NULL);
 		}
 		else
 		{
-			puts("messge sent");
+			puts("message sent!");
 			free(ioInfo);
 		}
 	}
-		return 0;
+	return 0;
 }
 
-void ErrorHandling(char* msg)
+void ErrorHandling(char* message)
 {
-	fputs(msg, stderr);
+	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
 }
